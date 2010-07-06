a557499aa43109220a8f33baaf0dbe5482b7a08f
Micah Ransdell <mjr578@gmail.com>
2010-07-05 20:46:35 -0700

    Added option parsing and starting/stopping
      - Now able to add options (using optparse) but am not using options, just args
      - Starting and Stopping the daemon now possible
        - Pass start or stop to command
      - Friendlier notices so users don't see ugly python errors

--- trans-notify/notify.py
+++ trans-notify/notify.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import transmissionrpc
+import os
+import sys
+import signal
 import datetime
 import time
 import notifo
+import transmissionrpc
 import daemon
+from optparse import OptionParser
 
 # Change these values to match your setup
 TRANSMISSION_USER = 'admin'
 TRANSMISSION_PASSWORD = ''
 TRANSMISSION_URL = 'localhost'
 TRANSMISSION_PORT = 9091
-
 NOTIFO_USER = '[Personal Notifo.com Username]'
 NOTIFO_KEY = '[Personal Notifo.com Key]'
+
+# Time in seconds between checking for changes
+# Change this if you would like to poll less or more frequently
+CHECK_INTERVAL = 30
+
+# Feel free to change these values if you want a different notice on your Phone
 NOTIFO_LABEL = 'TRANS-NOTIFY'
 NOTIFO_TRANS_FINISHED = 'Download Finished'
 NOTIFO_TRANS_STARTED = 'Download Started'
 
-# Time in seconds between checking for changes
-CHECK_INTERVAL = 30
+# Don't bother with this one
+PID_FILE_LOCATION =  '/tmp/trans-notify.pid'
+
+
 DELTA = datetime.timedelta(seconds=CHECK_INTERVAL)
 
-def run():
-    tc = transmissionrpc.Client(TRANSMISSION_URL, port=TRANSMISSION_PORT,
-                                user=TRANSMISSION_USER,
-                                password=TRANSMISSION_PASSWORD)
-
+def run(tc):
     torrents = tc.list()
     notify = notifo.Notifo(NOTIFO_USER, NOTIFO_KEY)
 
@@ -44,8 +51,46 @@ def run():
                                      label=NOTIFO_LABEL,
                                      title=NOTIFO_TRANS_STARTED)
 
-if __name__ == '__main__':
-    daemon.daemonize('/tmp/trans-notify.pid')
+def startup():
+    print 'Starting up Trans-Notify'
+    try:
+        tc = transmissionrpc.Client(TRANSMISSION_URL, port=TRANSMISSION_PORT,
+                                user=TRANSMISSION_USER,
+                                password=TRANSMISSION_PASSWORD)
+    except transmissionrpc.transmission.TransmissionError:
+        print 'Please set the location of Transmission and the proper username and password'
+        sys.exit(1)
+    daemon.daemonize(PID_FILE_LOCATION)
     while True:
-        run()
+        run(tc)
         time.sleep(CHECK_INTERVAL)
+
+def shutdown():
+    try:
+        pid = open(PID_FILE_LOCATION).read()
+    except IOError:
+        print 'Could not find PID file. Perhaps you already killed the process?'
+        sys.exit(0)
+    try:
+        os.kill(int(pid), signal.SIGKILL)
+    except OSError:
+        print 'Trans-Notify is not currently running'
+    os.remove(PID_FILE_LOCATION)
+    print 'Killed Trans-Notify'
+
+if __name__ == '__main__':
+    usage = "usage: %prog start|stop"
+
+    parser = OptionParser(usage)
+    
+    (options, args) = parser.parse_args()
+
+    if len(args) < 1:
+        parser.error("No action specified")
+    else:
+        if args[0].lower() == 'start':
+            startup()
+        else:
+            shutdown()
+
+
